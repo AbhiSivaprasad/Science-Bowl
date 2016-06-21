3c8a4d13af417db0a73353176a39e5dcb4e1007f
Abhi <abhi@live.com>
2016-06-21 10:25:27 -0700

    Correct and incorrect answers light up. Added an icon to next button.

--- app/src/main/java/com/abhi/android/sciencebowl/MainActivity.java
+++ app/src/main/java/com/abhi/android/sciencebowl/MainActivity.java
@@ -1,5 +1,8 @@
 package com.abhi.android.sciencebowl;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +16,7 @@ import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -23,6 +27,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button mChoiceX;
     private Button mChoiceY;
     private Button mChoiceZ;
+    private Button[] mChoiceButtonList; //List of buttons eases setEnabled and correct/incorrectChoice buttons.
+
     private Button mNextButton;
 
     private List<Question> mQuestionBank = new ArrayList<Question>();
@@ -35,7 +41,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         Firebase.setAndroidContext(this);
-
         //Takes some time to get data. Executes subsequent code before data is retrieved causing a lag between inflation of
         //XML and the appearance of question/answers. Need to fix this.
         mFirebaseRef = new Firebase("https://science-bowl.firebaseio.com/quizlist");
@@ -63,56 +68,81 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mChoiceY = (Button) findViewById(R.id.choiceY);
         mChoiceZ = (Button) findViewById(R.id.choiceZ);
 
-        mChoiceW.setOnClickListener(this);
-        mChoiceX.setOnClickListener(this);
-        mChoiceY.setOnClickListener(this);
-        mChoiceZ.setOnClickListener(this);
+        mChoiceButtonList = new Button[] {mChoiceW, mChoiceX, mChoiceY, mChoiceZ};
+        for(Button choiceButton : mChoiceButtonList)
+            choiceButton.setOnClickListener(this);
 
+        //attach listener to NextButton and make it initially invisible
         mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setVisibility(View.GONE);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mCurrQuestionIndex = (mCurrQuestionIndex + 1) % mQuestionBank.size(); //questions fed in circular loop. Need more sophisticated order.
                 updateQuestion();
-                choiceButtonsEnabled(true);
             }
         });
+
     }
 
     //update UI and mCurrQuestion
     private void updateQuestion() {
+        mNextButton.setVisibility(View.GONE);
         mCurrQuestion = mQuestionBank.get(mCurrQuestionIndex);
         mQuestion.setText(mCurrQuestion.getQuestion());
         mChoiceW.setText("W) " + mCurrQuestion.getW());
         mChoiceX.setText("X) " + mCurrQuestion.getX());
         mChoiceY.setText("Y) " + mCurrQuestion.getY());
         mChoiceZ.setText("Z) " + mCurrQuestion.getZ());
+
+        for(Button choiceButton : mChoiceButtonList)
+            choiceButton.setBackgroundColor(Color.LTGRAY);
+
+        choiceButtonsEnabled(true);
     }
 
     @Override
     public void onClick(View view) { //clean up this method
         Button selectedChoice = (Button) view;
-        String answer = mCurrQuestion.getCorrect();
 
         char choiceLetter = ((String) selectedChoice.getText()).charAt(0); // Letter comes first in answer choice
-        boolean isCorrect = (answer.toUpperCase().charAt(0) == choiceLetter);
+        char answer = mCurrQuestion.getCorrect().toUpperCase().charAt(0);
+
+        boolean isCorrect = (answer == choiceLetter);
+
+        Button correctChoice = getChoiceButton(answer);
+        correctChoice.setBackgroundColor(Color.GREEN); //correct answer effect always shown
 
         String result;
         if(isCorrect)
             result = "Correct";
-        else
+        else {
             result = "Incorrect";
-        Toast t = Toast.makeText(MainActivity.this , result, Toast.LENGTH_SHORT);
-        t.show();
+            Button inCorrectChoice = (Button) getChoiceButton(choiceLetter);
+            inCorrectChoice.setBackgroundColor(Color.RED); //incorrect answer effect
+        }
 
+        Toast t = Toast.makeText(this , result, Toast.LENGTH_SHORT);
+        t.show(); //turn this to actual display on screen
+
+        mNextButton.setVisibility(View.VISIBLE);
         choiceButtonsEnabled(false);
+    }
+
+    private Button getChoiceButton(char letter)
+    {
+        switch(letter) {
+            case 'W': return mChoiceW;
+            case 'X': return mChoiceX;
+            case 'Y': return mChoiceY;
+            case 'Z': return mChoiceZ;
+            default:  return null; //throw an error instead
+        }
     }
 
     private void choiceButtonsEnabled(boolean isEnabled)
     {
-        mChoiceW.setEnabled(isEnabled);
-        mChoiceX.setEnabled(isEnabled);
-        mChoiceY.setEnabled(isEnabled);
-        mChoiceZ.setEnabled(isEnabled);
+        for(Button choiceButton : mChoiceButtonList)
+            choiceButton.setEnabled(isEnabled);
     }
 }
