69f52cb0599c02d5e57535c0ae52907ecbddf5df
Abhi <abhi@live.com>
2016-06-21 18:02:55 -0700

    Added XML for main menu

--- app/src/main/java/com/abhi/android/sciencebowl/MainActivity.java
+++ app/src/main/java/com/abhi/android/sciencebowl/MainActivity.java
@@ -82,7 +82,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 updateQuestion();
             }
         });
-
     }
 
     //update UI and mCurrQuestion
