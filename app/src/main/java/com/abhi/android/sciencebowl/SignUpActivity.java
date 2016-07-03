4309e7ff4894eb888f4d3ac5c4e112e3d29b5ceb
Tanuj <tanujnay112@live.com>
2016-07-03 13:10:53 -0700

    Changed theme

--- app/src/main/java/com/abhi/android/sciencebowl/SignUpActivity.java
+++ app/src/main/java/com/abhi/android/sciencebowl/SignUpActivity.java
@@ -3,8 +3,6 @@ package com.abhi.android.sciencebowl;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
