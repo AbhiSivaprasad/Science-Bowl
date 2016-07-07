ea794105e54570ce452dd7bc70da31a92ecd29fc
Abhi <abhi@live.com>
2016-07-07 16:54:52 -0700

    added play online button, PlayOnlineActivity, and PlayOnlineSetupActivity

---
+++ app/src/main/java/com/abhi/android/sciencebowl/PlayOnlineActivity.java
@@ -0,0 +1,14 @@
+package com.abhi.android.sciencebowl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayOnlineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_online);
+
+    }
+}
